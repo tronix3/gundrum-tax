e0fae66b033a32b953294d1e1d17be686220d34e
Jeff <20178164+tronix3@users.noreply.github.com>
2025-02-26 22:16:12 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/Transparent-02.png" alt="Gundrum Logo" className={styles.logo} />
+      
       </footer>
     </>
   )
